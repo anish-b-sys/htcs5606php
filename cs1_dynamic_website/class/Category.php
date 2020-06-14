97fdf61f5491cb6fc532d5278e9b2ff451a14fec
anish-b-sys <bhalla04@myunitec.ac.nz>
2020-06-15 09:57:34 +1200

    php

---
+++ cs1_dynamic_website/class/Category.php
@@ -0,0 +1,21 @@
+
+<?php
+class Category
+{
+    public $id;
+    public $name;
+    public $picture;
+
+    /**
+     * Category constructor.
+     * @param $id
+     * @param $name
+     * @param $picture
+     */
+    public function __construct($id, $name, $picture)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->picture = $picture;
+    }
+}
---
+++ cs1_dynamic_website/class/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class Product
+{
+    public $id;
+    public $name;
+    public $price;
+    public $picture;
+    public $description;
+    public $categoryID;
+
+    /**
+     * Product constructor.
+     * @param $id
+     * @param $name
+     * @param $price
+     * @param $description
+     * @param $categoryID
+     */
+    public function __construct($id, $name, $price, $picture, $description, $categoryID)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->picture = $picture;
+        $this->description = $description;
+        $this->categoryID = $categoryID;
+    }
+
+
+}
